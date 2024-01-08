8015e3e982b1e25a8c141e253c441174e0346aec
Kostas Karayannis <kkarayannis@gmail.com>
2024-01-08 16:20:35 +0100

    Initial Commit

---
+++ Crypto Price Tracker/Crypto_Price_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Price_TrackerApp.swift
+//  Crypto Price Tracker
+//
+//  Created by Konstadinos Karayannis on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Price_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
